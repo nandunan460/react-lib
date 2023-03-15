9a3cd3c5bf31cfddbaa39dff6d4941ea50c2ce46
Nandini <nandini.gottumukkala@pepsico.com>
2023-03-15 17:01:58 +0530

    second commit

--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -1,10 +1,11 @@
-import React from "react"
-import "./Button.css"
+import React from "react";
 
-interface ButtonProps{
-    label:string;
+export interface ButtonProps {
+  label: string;
 }
-const Button=(props:ButtonProps)=>{
-    <button>{props.label}</button>
-}
+
+const Button = (props: ButtonProps) => {
+  return <button>{props.label}</button>;
+};
+
 export default Button;
